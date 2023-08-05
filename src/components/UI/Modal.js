8f95397e640355f746944bd1af41442683074ea1
lauraMelita <melita_laura@hotmail.com>
2023-08-05 12:40:40 +0300

    Created a reusable modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './Modal.scss';
 
-const Modal = () => {
-  return <></>;
+const portalElement = document.getElementById('overlays');
+
+const Backdrop = (props) => {
+  return <div className='backdrop' onClick={props.close} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className='modal'>
+      <div className='Content'>{props.children}</div>
+      <button onClick={props.close}>Close</button>
+    </div>
+  );
+};
+
+const Modal = ({ isOpen, close, children }) => {
+  return (
+    <>
+      {isOpen
+        ? ReactDOM.createPortal(<Backdrop close={close} />, portalElement)
+        : null}
+      {isOpen
+        ? ReactDOM.createPortal(
+            <ModalOverlay close={close}>{children}</ModalOverlay>,
+            portalElement
+          )
+        : null}
+    </>
+  );
 };
 
 export default Modal;
--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { useModal } from '../../hooks/useModal';
+
+import Modal from '../UI/Modal';
+
 import './Footer.scss';
 
 const Footer = () => {
-  return <footer />;
+  const { isOpen, toggle } = useModal();
+
+  return (
+    <footer>
+      <button onClick={toggle}>Contact</button>
+      <Modal isOpen={isOpen} close={toggle}>
+        <form />
+      </Modal>
+    </footer>
+  );
 };
 
 export default Footer;
