84df9d76a4d387a3c54ec72f5508caa2639136fb
lauraMelita <melita_laura@hotmail.com>
2023-12-02 19:34:09 +0100

    Restructured layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,20 +5,22 @@ import About from './about/About';
 import Quote from './quote/Quote';
 import HomeCategories from './categories/HomeCategories';
 
+import './Home.scss';
+
 const HomePage = () => {
   return (
-    <main>
+    <main className='home'>
       <section id='parallax'>
         <ParallaxContainer />
+      </section>
+      <section id='categories'>
+        <HomeCategories />
       </section>
       <section id='about'>
         <About />
       </section>
       <section id='quote'>
         <Quote />
-      </section>
-      <section id='categories'>
-        <HomeCategories />
       </section>
     </main>
   );
