8e69e92f8ef8de1d50ea2bcb1974494bffe47729
lauraMelita <melita_laura@hotmail.com>
2023-12-17 20:03:04 +0100

    Defined props for the reusable parallax layers

--- src/pages/home/parallax/parallaxLayer/ParallaxLayer.jsx
+++ src/pages/home/parallax/parallaxLayer/ParallaxLayer.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const ParallaxLayer = ({ layer, src, layerData }) => {
+const ParallaxLayer = ({
+  layer,
+  stackOrder,
+  transformY,
+  transformX,
+  src,
+  size,
+  position,
+}) => {
   return (
     <motion.div
       className={`parallax__${layer}`}
       style={{
+        zIndex: stackOrder,
+        y: transformY,
+        x: transformX,
         backgroundImage: `url(${src})`,
+        backgroundSize: size,
+        backgroundPosition: position,
+        backgroundRepeat: 'no-repeat',
       }}
     />
   );
