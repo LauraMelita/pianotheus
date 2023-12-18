c54d98fa853c9a0e4bcb18eccf62449b4caafe8e
lauraMelita <melita_laura@hotmail.com>
2023-12-18 20:23:53 +0100

    Small unrelated correction

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,37 +1,37 @@
 export const siteConfig = {
   firestoreCollections: [
     { name: 'movies', orderBy: 'title' },
-    { name: 'tv-shows', orderBy: 'title' },
-    { name: 'video-games', orderBy: 'title' },
+    { name: 'tv_shows', orderBy: 'title' },
+    { name: 'video_games', orderBy: 'title' },
     { name: 'classical', orderBy: 'composer' },
   ],
   navLinks: [
     { path: '/', navLink: 'Home' },
     { path: '/movies', navLink: 'Movies' },
-    { path: '/tv-shows', navLink: 'TV Shows' },
+    { path: '/tv_shows', navLink: 'TV Shows' },
     { path: '/video-games', navLink: 'Video Games' },
     { path: '/classical', navLink: 'Classical Music' },
   ],
   categories: [
     {
       category: 'Movies',
-      backgroundImage: 'movies-bg.webp',
-      popOutImage: 'movies-popout.webp',
+      backgroundImage: 'movies_bg.webp',
+      popOutImage: 'movies_popout.webp',
     },
     {
       category: 'TV Shows',
-      backgroundImage: 'tv-shows-bg.webp',
-      popOutImage: 'tv-shows-popout.webp',
+      backgroundImage: 'tv-shows_bg.webp',
+      popOutImage: 'tv-shows_popout.webp',
     },
     {
       category: 'Video Games',
-      backgroundImage: 'video-games-bg.webp',
-      popOutImage: 'video-games-popout.webp',
+      backgroundImage: 'video-games_bg.webp',
+      popOutImage: 'video-games_popout.webp',
     },
     {
       category: 'Classical',
-      backgroundImage: 'classical-bg.webp',
-      popOutImage: 'classical-popout.webp',
+      backgroundImage: 'classical_bg.webp',
+      popOutImage: 'classical_popout.webp',
     },
   ],
 };
