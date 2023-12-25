7f67ce2d6a20be3acd68491a8cb3ff0b355d3e2c
lauraMelita <melita_laura@hotmail.com>
2023-12-25 12:23:59 +0100

    Restructured the re-usable navigation links component

--- src/components/common/navbar/NavigationLinks.jsx
+++ src/components/common/navbar/NavigationLinks.jsx
@@ -1,53 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { motion } from 'framer-motion';
+
+import Svg from '../../UI/svg/Svg';
 
 import { siteConfig } from '../../../utils/config';
 
-import { useScrollToTop } from '../../../hooks/useScrollToTop';
-
-const NavigationLinks = (props) => {
-  const { handlePageScroll } = useScrollToTop();
-
-  const variants = {
-    open: {
-      transition: {
-        staggerChildren: 0.1,
-      },
-    },
-    closed: {
-      transition: {
-        staggerChildren: 0.05,
-        staggerDirection: -1,
-      },
-    },
-  };
-  const itemVariants = {
-    open: {
-      y: 0,
-      opacity: 1,
-    },
-    closed: {
-      y: 50,
-      opacity: 0,
-    },
-  };
-
+const NavigationLinks = ({ className, shouldRenderIcons, onClick }) => {
   return (
-    <motion.ul className={props.className} variants={variants}>
-      {siteConfig.navLinks.map(({ path, navLink }) => (
-        <motion.li
-          key={navLink}
-          variants={itemVariants}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <NavLink to={path} onClick={(e) => handlePageScroll(e)}>
+    <ul className={className}>
+      {siteConfig.navLinks.map(({ path, navLink, icon }) => (
+        <li key={navLink}>
+          <NavLink to={path} onClick={onClick}>
+            {shouldRenderIcons && <Svg icon={icon} />}
             {navLink}
           </NavLink>
-        </motion.li>
+        </li>
       ))}
-    </motion.ul>
+    </ul>
   );
 };
 
