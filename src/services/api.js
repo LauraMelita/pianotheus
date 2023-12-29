4c3933920f0576cee146e05a97bbebe9975a2af3
lauraMelita <melita_laura@hotmail.com>
2023-12-29 22:47:31 +0100

    Installed axios and created get functions for the rawg and imdb api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import axios from 'axios';
+
 import { db } from './firebase';
+import { OMDB_API, RAWG_URL } from '../utils/constants';
 
 export const fetchCollection = async (collectionName, orderCollectionBy) => {
   const collectionRef = collection(db, collectionName);
@@ -17,4 +20,20 @@ export const fetchCollection = async (collectionName, orderCollectionBy) => {
   }));
 
   return documents;
+};
+
+export const fetchImdbData = async (imdbId) => {
+  const { data } = await axios.get(
+    `${OMDB_API}/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${imdbId}`
+  );
+
+  return data;
+};
+
+export const fetchRawgData = async (rawgId) => {
+  const { data } = await axios.get(
+    `${RAWG_URL}/games/${rawgId}?key=${process.env.REACT_APP_RAWG_API_KEY}`
+  );
+
+  return data;
 };
