8add375f1ac1a573a28768c0119b56b6bc7771a4
lauraMelita <melita_laura@hotmail.com>
2024-01-21 17:47:36 +0100

    Created various re-usable form components

---
+++ src/components/UI/form/PasswordInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { useShowPassword } from '../../../hooks/useShowPassword';
+
+import { FormGroup, Label, Input } from './Form';
+import Button from '../button/Button';
+import Svg from '../svg/Svg';
+import Alert from '../alert/Alert';
+
+const PasswordInput = ({ register, errors }) => {
+  const { inputType, toggleShowPassword, icon } = useShowPassword();
+  return (
+    <FormGroup className='password'>
+      <Label>Password</Label>
+      <div>
+        <Input {...register('password')} type={inputType} name='password' />
+        <Button variant='icon' type='button' onClick={toggleShowPassword}>
+          <Svg icon={icon} />
+        </Button>
+      </div>
+      <Alert severity='error'>
+        {errors.password && errors.password.message}
+      </Alert>
+    </FormGroup>
+  );
+};
+
+export default PasswordInput;
---
+++ src/components/UI/form/SubmitButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Button from '../button/Button';
+import Spinner from '../spinner/Spinner';
+
+const SubmitButton = ({ btnText, isSubmitting }) => {
+  return (
+    <Button disabled={isSubmitting} variant='default' type='submit'>
+      {isSubmitting ? (
+        <>
+          {btnText}
+          <Spinner type='dotted' />
+        </>
+      ) : (
+        btnText
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
