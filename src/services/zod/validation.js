a5e6d3ca8d8a4732432582b84f079efcc4f929bb
lauraMelita <melita_laura@hotmail.com>
2024-01-21 17:46:24 +0100

    Improved form validation for the sign in, register and contact forms

--- src/services/zod/validation.js
+++ src/services/zod/validation.js
@@ -1,17 +1,84 @@
 import { z } from 'zod';
 
+import { FORM } from '../../utils/constants';
+import { isValueSafe } from '../../utils/helpers';
+import { bytesToMegabytes, fileExtensionsList } from '../../utils/formatting';
+
 export const signUpValidation = z.object({
   username: z
     .string()
-    .min(2, { message: 'Username must be at least 2 characters' }),
+    .min(FORM.MIN_TEXT_LENGTH, {
+      message: `Username must be at least ${FORM.MIN_TEXT_LENGTH} characters`,
+    })
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
   email: z.string().email('Email is not valid'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-  //   avatar: ?
+  password: z
+    .string()
+    .min(
+      FORM.MIN_PASSWORD_LENGTH,
+      `Password must be at least ${FORM.MIN_PASSWORD_LENGTH} characters`
+    )
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
+  file: z
+    .any()
+    .refine((file) => file?.length !== 0, 'An image is required')
+    .refine(
+      (file) => file[0]?.size <= FORM.MAX_FILE_SIZE,
+      `Invalid file. The maximum image size is ${bytesToMegabytes(
+        FORM.MAX_FILE_SIZE
+      )} MB`
+    )
+    .refine(
+      (file) => FORM.ACCEPTED_IMAGE_TYPES.includes(file[0]?.type),
+      `Invalid file. Only ${fileExtensionsList(
+        FORM.ACCEPTED_IMAGE_TYPES
+      )} formats are supported`
+    ),
 });
 
 export const signInValidation = z.object({
   email: z.string().email('Email is not valid'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
+  password: z
+    .string()
+    .min(
+      FORM.MIN_PASSWORD_LENGTH,
+      `Password must be at least ${FORM.MIN_PASSWORD_LENGTH} characters`
+    )
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
 });
 
-export const contactValidation = z.object({});
+export const contactValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: 'Name must be at least 2 characters' })
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
+  email: z.string().email('Email is not valid'),
+  subject: z
+    .string()
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
+  message: z
+    .string()
+    .max(
+      FORM.MAX_MESSAGE_LENGTH,
+      `Message cannot exceed ${FORM.MAX_MESSAGE_LENGTH} characters`
+    )
+    .refine(
+      (value) => isValueSafe(value),
+      'HTML or script tags are not allowed'
+    ),
+});
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,8 +6,17 @@ export const API = {
   RAWG: 'https://rawg.io/api',
 };
 
-// File extensions
+// App file extensions
 export const EXTENSION = {
   MIDI: 'mid',
   PDF: 'pdf',
+};
+
+// Form validation
+export const FORM = {
+  MIN_TEXT_LENGTH: 2,
+  MIN_PASSWORD_LENGTH: 6,
+  MAX_MESSAGE_LENGTH: 1000,
+  MAX_FILE_SIZE: 5000000,
+  ACCEPTED_IMAGE_TYPES: ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'],
 };
