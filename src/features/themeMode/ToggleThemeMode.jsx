b282893207f1867805e870974e3d2129a2daf7ba
lauraMelita <melita_laura@hotmail.com>
2024-01-27 16:28:09 +0100

    Restructured toggle theme component

--- src/features/themeMode/ToggleThemeMode.jsx
+++ src/features/themeMode/ToggleThemeMode.jsx
@@ -5,12 +5,17 @@ import { useThemeContext } from '../../context/ThemeContext';
 import Button from '../../components/UI/button/Button';
 import Svg from '../../components/UI/svg/Svg';
 
-const ToggleThemeMode = () => {
-  const { theme, toggleTheme } = useThemeContext();
+import { capitalize } from '../../utils/formatting';
+
+const ToggleThemeMode = ({ onClick }) => {
+  const { theme } = useThemeContext();
+
+  const switchTheme = capitalize(theme === 'dark' ? 'light' : 'dark');
 
   return (
-    <Button className='toggle-theme-btn' variant='icon' onClick={toggleTheme}>
+    <Button className='toggle-theme-btn' variant='icon-text' onClick={onClick}>
       <Svg icon={theme === 'dark' ? 'sun' : 'moon'} />
+      {switchTheme} Mode
     </Button>
   );
 };
