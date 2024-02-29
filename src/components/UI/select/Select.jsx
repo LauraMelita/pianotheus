392ccf7653f58959a45c8793d7e888d3c2beadc6
lauraMelita <melita_laura@hotmail.com>
2024-02-29 14:11:27 +0200

    Created a reusable select component

--- src/components/UI/select/Select.jsx
+++ src/components/UI/select/Select.jsx
@@ -2,36 +2,20 @@ import React, { forwardRef } from 'react';
 import * as RadixSelect from '@radix-ui/react-select';
 
 import Scrollbar from '../scrollbar/Scrollbar';
-
 import Svg from '../svg/Svg';
+
 import './Select.scss';
 
-const Select = ({ options, value, onChange }) => {
-  console.log(options);
-
-  const SelectItem = forwardRef(
-    ({ children, className, ...props }, forwardedRef) => {
-      return (
-        <RadixSelect.Item
-          className='select__item'
-          {...props}
-          ref={forwardedRef}
-        >
-          <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
-        </RadixSelect.Item>
-      );
-    }
-  );
-
-  return (
-    <div className='select'>
-      {/* <span className='select__label'>{options.option}</span> */}
-      <RadixSelect.Root value={value} onValueChange={onChange}>
+const Select = forwardRef(
+  ({ label, value, onChange, children, ...props }, ref) => {
+    return (
+      <RadixSelect.Root value={value} onValueChange={onChange} {...props}>
         <RadixSelect.Trigger
+          ref={ref}
           className='select__trigger'
-          // aria-label={options.option}
+          aria-label={label}
         >
-          <RadixSelect.Value placeholder='Composer' aria-label={value} />
+          <RadixSelect.Value placeholder={label} aria-label={value} />
           <RadixSelect.Icon className='select__icon'>
             <Svg icon='chevron-down' />
           </RadixSelect.Icon>
@@ -44,20 +28,26 @@ const Select = ({ options, value, onChange }) => {
         >
           <Scrollbar>
             <RadixSelect.Viewport asChild>
-              <RadixSelect.Group>
-                {value && <SelectItem value='all'>All</SelectItem>}
-                {options.values.map((value, index) => (
-                  <SelectItem key={index} value={value}>
-                    {value}
-                  </SelectItem>
-                ))}
-              </RadixSelect.Group>
+              <RadixSelect.Group>{children}</RadixSelect.Group>
             </RadixSelect.Viewport>
           </Scrollbar>
         </RadixSelect.Content>
       </RadixSelect.Root>
-    </div>
-  );
-};
+    );
+  }
+);
 
-export default Select;
+const Option = forwardRef(({ label, value, ...props }, ref) => {
+  return (
+    <RadixSelect.Item
+      ref={ref}
+      className='select__item'
+      value={value}
+      {...props}
+    >
+      <RadixSelect.ItemText>{label}</RadixSelect.ItemText>
+    </RadixSelect.Item>
+  );
+});
+
+export { Select, Option };
