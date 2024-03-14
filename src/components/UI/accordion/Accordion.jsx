327fd0b85f5369ee5ab71eec10b6f54f63ac7a42
lauraMelita <melita_laura@hotmail.com>
2024-03-14 12:49:02 +0200

    Improved the accordion component

--- src/components/UI/accordion/Accordion.jsx
+++ src/components/UI/accordion/Accordion.jsx
@@ -5,36 +5,63 @@ import Svg from '../svg/Svg';
 
 import './Accordion.scss';
 
-const Accordion = ({ className, children }) => (
-  <RadixAccordion.Root className={`${className} accordion`} type='multiple'>
-    {children}
-  </RadixAccordion.Root>
-);
-
-const AccordionItem = forwardRef(({ value, children, ...props }, ref) => (
-  <RadixAccordion.Item
-    ref={ref}
-    className='accordion__item'
-    value={value}
-    {...props}
-  >
-    {children}
-  </RadixAccordion.Item>
-));
-
-const Trigger = forwardRef(({ children, ...props }, ref) => (
-  <RadixAccordion.Header className='accordion__header'>
-    <RadixAccordion.Trigger className='accordion__trigger' ref={ref} {...props}>
+const Accordion = ({
+  className,
+  type,
+  value,
+  defaultValue,
+  onValueChange,
+  collapsible,
+  children,
+}) => {
+  return (
+    <RadixAccordion.Root
+      className={`${className} accordion`}
+      type={type}
+      value={value}
+      defaultValue={defaultValue}
+      onValueChange={onValueChange}
+      collapsible={collapsible}
+    >
       {children}
-      <Svg icon='chevron-down' aria-hidden />
-    </RadixAccordion.Trigger>
-  </RadixAccordion.Header>
-));
+    </RadixAccordion.Root>
+  );
+};
 
-const Content = forwardRef(({ children, ...props }, ref) => (
-  <RadixAccordion.Content ref={ref} className='accordion__content' {...props}>
-    <div>{children}</div>
-  </RadixAccordion.Content>
-));
+const AccordionItem = forwardRef(({ value, children, ...props }, ref) => {
+  return (
+    <RadixAccordion.Item
+      ref={ref}
+      className='accordion__item'
+      value={value}
+      {...props}
+    >
+      {children}
+    </RadixAccordion.Item>
+  );
+});
+
+const Trigger = forwardRef(({ children, ...props }, ref) => {
+  return (
+    <RadixAccordion.Header className='accordion__header'>
+      <RadixAccordion.Trigger
+        className='accordion__trigger'
+        ref={ref}
+        {...props}
+      >
+        {children}
+        <Svg icon='chevron-down' aria-hidden />
+      </RadixAccordion.Trigger>
+    </RadixAccordion.Header>
+  );
+});
+
+const Content = forwardRef(({ children, ...props }, ref) => {
+  return (
+    <RadixAccordion.Content ref={ref} className='accordion__content' {...props}>
+      <div>{children}</div>
+    </RadixAccordion.Content>
+  );
+});
 
 export { Accordion, AccordionItem, Trigger, Content };
