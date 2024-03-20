b549c387ea6b9598a368237b1c2a61ae5deddc7f
lauraMelita <melita_laura@hotmail.com>
2024-03-20 19:16:22 +0200

    Removed unused props

--- src/components/UI/image/Image.jsx
+++ src/components/UI/image/Image.jsx
@@ -1,17 +1,15 @@
 import React, { forwardRef } from 'react';
 
-const Image = forwardRef(
-  ({ className, srcSet, mediaQuery, src, alt, ...props }, ref) => {
-    return (
-      <img
-        ref={ref}
-        className={className && className}
-        src={src}
-        alt={alt}
-        {...props}
-      />
-    );
-  }
-);
+const Image = forwardRef(({ className, src, alt, ...props }, ref) => {
+  return (
+    <img
+      ref={ref}
+      className={className && className}
+      src={src}
+      alt={alt}
+      {...props}
+    />
+  );
+});
 
 export default Image;
