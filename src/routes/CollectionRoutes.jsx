572dce3146bc89089010b5747b130bcd3e36376f
lauraMelita <melita_laura@hotmail.com>
2024-04-17 11:05:30 +0300

    Used dynamic routes and followed react-router's guidelines for route nesting

--- src/routes/CollectionRoutes.jsx
+++ src/routes/CollectionRoutes.jsx
@@ -1,42 +1,49 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Routes, Route, Outlet, useParams } from 'react-router-dom';
 
 import { CollectionProvider } from '../context/CollectionContext';
 import { PlayerProvider } from '../context/PlayerContext';
+import { siteConfig } from '../utils/config';
 
 import ProtectedRoute from '../features/authentication/routing/ProtectedRoute';
 import CollectionPage from '../pages/collectionPage/CollectionPage';
 import ScoresPage from '../pages/scoresPage/ScoresPage';
+import ErrorPage from '../pages/errorPage/ErrorPage';
 
-import { siteConfig } from '../utils/config';
+const Providers = () => {
+  return (
+    <ProtectedRoute>
+      <CollectionProvider>
+        <PlayerProvider>
+          <Outlet />
+        </PlayerProvider>
+      </CollectionProvider>
+    </ProtectedRoute>
+  );
+};
 
 const CollectionRoutes = () => {
-  return siteConfig.collections.map(({ collection, routeParam }) => (
-    <Route key={collection} path={collection}>
-      <Route
-        index
-        element={
-          <ProtectedRoute>
-            <CollectionProvider>
-              <CollectionPage />
-            </CollectionProvider>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path={`:${routeParam}`}
-        element={
-          <ProtectedRoute>
-            <CollectionProvider>
-              <PlayerProvider>
-                <ScoresPage />
-              </PlayerProvider>
-            </CollectionProvider>
-          </ProtectedRoute>
-        }
-      />
-    </Route>
-  ));
+  const { collection: currentCollection } = useParams();
+
+  const collectionConfig = siteConfig.collections.find(
+    ({ collection }) => collection === currentCollection
+  );
+
+  if (collectionConfig) {
+    const { routeParam } = collectionConfig;
+
+    return (
+      <Routes>
+        <Route element={<Providers />}>
+          <Route index element={<CollectionPage />} />
+          <Route path={`:${routeParam}`} element={<ScoresPage />} />
+          <Route path='*' element={<ErrorPage code='404' />} />
+        </Route>
+      </Routes>
+    );
+  } else {
+    return <ErrorPage code='404' />;
+  }
 };
 
 export default CollectionRoutes;
