c94bdb0dbe674bd8fc6cb6ba4261b5bf4da08686
lauraMelita <melita_laura@hotmail.com>
2024-04-21 12:07:41 +0300

    Moved color animation into cutom hook for reusablity

--- src/components/UI/animation/aurora/Aurora.jsx
+++ src/components/UI/animation/aurora/Aurora.jsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react';
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  animate,
-} from 'framer-motion';
+import React from 'react';
+import { motion, useMotionTemplate } from 'framer-motion';
+
+import { useAnimatedColors } from '../../../../hooks/useAnimatedColors';
 
 import { GLOBAL_STYLES } from '../../../../utils/constants';
 
-const Aurora = ({ hexColors, duration = 10 }) => {
-  const color = useMotionValue(hexColors[0]);
+const Aurora = ({ hexColors }) => {
+  const color = useAnimatedColors(hexColors, 10);
+
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, ${GLOBAL_STYLES.TRANSPARENT_COLOR} 50%, ${color})`;
-
-  // ALSO AVAILABLE:
-  // const border = useMotionTemplate`1px solid ${color}`;
-  // const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
-
-  useEffect(() => {
-    animate(color, hexColors, {
-      ease: 'easeInOut',
-      duration,
-      repeat: Infinity,
-      repeatType: 'mirror',
-    });
-  }, []);
 
   return (
     <motion.div
