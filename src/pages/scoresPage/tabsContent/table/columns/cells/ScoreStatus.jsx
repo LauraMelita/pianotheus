856bec0b24c905680e5a6392788a0d4b340f26a9
lauraMelita <melita_laura@hotmail.com>
2024-06-11 17:48:23 +0300

    Removed tootlip for unavailable files

--- src/pages/scoresPage/tabsContent/table/columns/cells/ScoreStatus.jsx
+++ src/pages/scoresPage/tabsContent/table/columns/cells/ScoreStatus.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import InfoPopup from '../../../../../../features/infoPopup/InfoPopup';
 
 const ScoreStatus = ({ isUpcoming, isUnavailable }) => {
+  if (isUnavailable) return <span className='score__status'>---</span>;
+
   if (isUpcoming)
     return (
       <InfoPopup className='score__status' title='Coming Soon'>
@@ -12,19 +14,6 @@ const ScoreStatus = ({ isUpcoming, isUnavailable }) => {
           This score is currently being transcribed,
           <br />
           and will be available soon.
-        </p>
-      </InfoPopup>
-    );
-
-  if (isUnavailable)
-    return (
-      <InfoPopup className='score__status' title='Unavailable'>
-        <p>
-          Unfortunately, this file is unavailable
-          <br />
-          and might not be added in the future.
-          <br />
-          Please explore our other resources.
         </p>
       </InfoPopup>
     );
