58506a21587d8921d89094a12ed006c44f7eb7cc
lauraMelita <melita_laura@hotmail.com>
2024-06-20 13:54:11 +0300

    Styled the toaster to match website design

--- src/components/UI/toaster/Toaster.jsx
+++ src/components/UI/toaster/Toaster.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import { ToastContainer as Toast } from 'react-toastify';
 
+import Button from '../button/Button';
+import Svg from '../svg/Svg';
+
+import './Toaster.scss';
+
+const closeButton = () => {
+  return (
+    <Button className='toaster__close-btn' variant='icon'>
+      <Svg icon='close' style={{ width: 20, height: 20 }} />
+    </Button>
+  );
+};
+
 const Toaster = () => {
-  return <Toast position='bottom-right' />;
+  return (
+    <Toast
+      theme='dark'
+      position='bottom-right'
+      closeButton={closeButton}
+      icon={false}
+    />
+  );
 };
 
 export default Toaster;
